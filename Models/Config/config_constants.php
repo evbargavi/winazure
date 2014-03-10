62f83c16e3a93c15a4da78cee56c2cf605a3cdeb
evbargavi <bargavi@eventurers.com>
2014-03-10 17:46:21 +0530

    f

--- Models/Config/config_constants.php
+++ Models/Config/config_constants.php
@@ -6,7 +6,12 @@
  * 
  */
 define('SITE_TITLE', 'Register');
-define('SITE_PATH', 'http://'.$_SERVER['HTTP_HOST'].'/training/register/');
+if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] == '172.21.4.100'){
+	define('SITE_PATH', 'http://'.$_SERVER['HTTP_HOST'].'/training/register/');
+}else{
+	define('SITE_PATH', 'http://ithamor.azurewebsites.net/');
+}
+
 define('STYLE_PATH', SITE_PATH.'/WebResources/Styles/');
 define('SCRIPT_PATH', SITE_PATH.'/WebResources/Scripts/');
 
