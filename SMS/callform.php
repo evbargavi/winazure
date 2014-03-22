984939939ea85dc3918329048d1a312f58569cf2
evbargavi <bargavi@eventurers.com>
2014-03-22 17:27:56 +0530

    test

--- SMS/callform.php
+++ SMS/callform.php
@@ -1,16 +1,9 @@
-<?php
-    if (isset($_POST['makecall'])) {
-       header("Location:makecall.php");
-    }
-    elseif (isset($_POST['sendsms'])) {
-        header("Location:sendsms.php");
-    }
-?>
 <html>
 <head>
     <title>Automated call form</title>
 	<link rel='stylesheet' href='styles.css' />
 	<meta name='viewport' content='width=device-width' />
+	<script language="JavaScript" type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js"></script>
 </head>
 <body>
 <div id='wrapper'>
@@ -19,30 +12,34 @@
 			<h1>Automated Call Form</h1>
 			<h3>Fill in all fields and <b>Choose option to do action</b></h3>
 		</header>
-		<form method="post">
+		<form method="post" id="smsform" action="">
 			<table cellpadding="10" cellspacing="10" width="100%">
 			    <tr>
 			        <td width="20%">To:</td>
-			        <td><input type="text" size=50 name="callTo" value=""></td>
+			        <td><input type="text" size=50 name="callTo" value="" id="callTo"></td>
 			    </tr>
 			    <tr>
 			        <td>From:</td>
-			        <td><input type="text" size=50 name="callFrom" value=""></td>
-			    </tr>
+			        <td><input type="text" size=50 name="callFrom" value="" id="callFrom"></td>
+			    </tr>-
 			    <tr>
 			        <td>Call message:</td>
-			        <td><input type="text" size=100 name="callText" value="Hello. This is the call text. Good bye." /></td>
+			        <td><input type="text" size=100 name="callText" id="callText" value="Hello. This is the call text. Good bye." /></td>
 			    </tr>
 			    <tr>
-					<td colspan="2" align="center"><input type="submit" name="makecall" value="Make Voice SMS" style="margin-bottom:25px;">
-					<input type="submit" name="sendsms" value="Send Text SMS"></td>
+					<td colspan="2" align="center"><button name="makecall" id="makecall" style="margin-bottom:25px;" onclick="return postdataaction('makecall');">Make Voice SMS</button>
+					<button name="sendsms" id="sendsms" onclick="return postdataaction('sendsms');">Send Text SMS</button></td>
 			    </tr>
 			</table>
 		</form>
 	</article>
 </div>
 <script type="text/javascript">
-
+	function postdataaction(action){
+		$("#smsform").attr("action", action+".php");
+		$("#smsform").submit();
+		return true;
+	}
 </script>
 </body>
 </html>
